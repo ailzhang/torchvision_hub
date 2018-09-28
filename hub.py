fa6d7e62b3be69f0d48a5de1a02788288aa96555
Ailing Zhang <ailzhang@fb.com>
2018-09-28 10:45:27 -0700

    update key names

--- hub.py
+++ hub.py
@@ -1,13 +1,13 @@
 # /**************************For Repo Owner************************************
-# entrypoint_list is a tuple of (func_name, module_name, checkpoint_dir).
-#     func_name: required, this function should return a model
-#     module_name: required, specifies where to load the module in the package
+# entrypoints is a tuple of (func_name, module_name, checkpoint_dir).
+#     func_name: dependencies, this function should return a model
+#     module_name: dependencies, specifies where to load the module in the package
 #     checkpoint_url: optional, specifies where to download the checkpoint
-# deps_list is a list of dependent package names.
+# dependencies is a list of dependent package names.
 # help_msp is a string, explaining what model each entrypoint points to.
 # ****************************************************************************/
 
-entrypoint_list = [
+entrypoints = [
     ('wrapper1',
      'torchvision_hub.models.hub_example',
      'https://download.pytorch.org/models/resnet18-5c106cde.pth'),
@@ -15,18 +15,19 @@ entrypoint_list = [
      'torchvision_hub.transforms.hub_example',
      'https://download.pytorch.org/models/resnet18-5c106cde.pth')
     ]
-deps_list = ['torch', 'math']
+dependencies = ['torch', 'math']
 help_msg = ("/****** Hub Help Section ******/\n"
             "hello world\n"
             "/******  End of Message  ******/")
 
 
+# TODO: Move this to pytorch hub.py
 # /****************************************************************************
 # ********** Helper functions for Pytorch Hub *********************************
 # *************** (Please do not edit) ****************************************
 # ****************************************************************************/
 KEY_ENTRYPOINTS = 'entrypoints'
-KEY_REQUIRED = 'required'
+KEY_DEPENDENCIES = 'dependencies'
 KEY_CHECKPOINT = 'checkpoint'
 KEY_MODULE = 'module'
 KEY_HELP = 'help'
@@ -38,13 +39,13 @@ def add_entrypoint(hub_info, func_name, module_name, checkpoint_url=None):
             KEY_CHECKPOINT: checkpoint_url}
 
 
-def set_entrypoints(hub_info, entrypoint_list):
-    for func_name, module_name, checkpoint_url in entrypoint_list:
+def set_entrypoints(hub_info, entrypoints):
+    for func_name, module_name, checkpoint_url in entrypoints:
         add_entrypoint(hub_info, func_name, module_name, checkpoint_url)
 
 
-def set_required(hub_info, deps_list):
-    hub_info[KEY_REQUIRED] = deps_list
+def set_dependencies(hub_info, dependencies):
+    hub_info[KEY_DEPENDENCIES] = dependencies
 
 
 def set_help(hub_info, help_msg):
@@ -52,9 +53,9 @@ def set_help(hub_info, help_msg):
 
 
 def get_hub_info():
-    hub_info = {KEY_ENTRYPOINTS: {}, KEY_REQUIRED: [], KEY_HELP: ''}
-    set_entrypoints(hub_info, entrypoint_list)
-    set_required(hub_info, deps_list)
+    hub_info = {KEY_ENTRYPOINTS: {}, KEY_DEPENDENCIES: [], KEY_HELP: ''}
+    set_entrypoints(hub_info, entrypoints)
+    set_dependencies(hub_info, dependencies)
     set_help(hub_info, help_msg)
     return hub_info
 
